020f6c548d157489523bb2edb773e74b46ac9be0
nexuster <coralpinklmao@outlook.com>
2025-03-24 09:55:25 -0400

    ack ow

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let counter = 0;
+
+function assignUniqueId(element, prefix) {
+    counter++;
+    element.id = prefix + counter;
+}
+
 window.onload = function() {
     const speakerEntry = document.querySelector('#speaker');
     const submitButton = document.querySelector('#submitSpeaker');
@@ -5,28 +12,32 @@ window.onload = function() {
     const speakers = document.querySelector('#speakers');
 
     submitButton.addEventListener('click', function(e) {
-        addValueToDropdown(entrySpeaker,speakerEntry);
+        if (entrySpeaker.value.trim() === "") {
+            alert(dropdown.text + 'is not valid');
+            return
+        }
+        let newSpeaker = addValueToDropdown(entrySpeaker,speakerEntry);
         let newOutput = document.createElement('div');
-        newOutput.textcontent = entrySpeaker.value;
+        newOutput.textcontent = speakerEntry.value;
         let deleteButton = document.createElement('button');
-        deleteButton.text = '-'
+        deleteButton.textcontent = '-'
         newOutput.appendChild(deleteButton);
         speakers.appendChild(newOutput);
         deleteButton.addEventListener('click', function(e) {
             newOutput.remove();
+            newSpeaker.remove();
         });
     });
 }
 
 function addValueToDropdown(dropdown,entry) {
-    if (dropdown.value.trim() === "") {
-        alert(dropdown.text + 'is not valid');
-    } else {
-        let newSpeaker = document.createElement('option');
-        let speaker = entry.value;
-        newSpeaker.text = speaker;
-        newSpeaker.value = speaker;
-        dropdown.appendChild(newSpeaker);
-    }
+    let newSpeaker = document.createElement('option');
+    assignUniqueId(newSpeaker, 'speaker-')
+    let speaker = entry.value;
+    newSpeaker.text = speaker;
+    newSpeaker.value = speaker;
+    dropdown.appendChild(newSpeaker);
+
     entry.value == '';
+    return newSpeaker;
 }
