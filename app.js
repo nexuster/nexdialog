30c6f181a8db4b7f05b3737a25aa0d98d838a3ac
nexuster <coralpinklmao@outlook.com>
2025-03-24 08:48:53 -0400

    i think i did it wrong

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const entrySpeaker = document.querySelector('#entrySpeaker');
 if (submitButton.textContent != "") {
     submitButton.addEventListener('click', function(e) {
         let newSpeaker = document.createElement('option');
-        newSpeaker.textContent = speakerEntry.textContent;
-        newSpeaker.value = speakerEntry.textContent;
-        //speakerEntry.textContent = "";
-        entrySpeaker.appendChild(speakerEntry);
+        let speaker = speakerEntry.text;
+        newSpeaker.text = speaker;
+        newSpeaker.value = speaker;
+        speakerEntry.text = "";
+        entrySpeaker.add(speakerEntry);
     });
 } else {
     alert('invalid name content');
