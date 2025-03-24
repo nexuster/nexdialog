6b2d717459e39f1c7453d662fc972e2705978fd7
nexuster <coralpinklmao@outlook.com>
2025-03-24 09:46:52 -0400

    preview stuffs

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ window.onload = function() {
     const speakerEntry = document.querySelector('#speaker');
     const submitButton = document.querySelector('#submitSpeaker');
     const entrySpeaker = document.querySelector('#entrySpeaker');
+    const speakers = document.querySelector('#speakers');
 
     submitButton.addEventListener('click', function(e) {
-        if (entrySpeaker.value.trim() == "") {
-            alert(entrySpeaker.text + 'is not a valid speaker') //i HATE this
-        } else {
-            let newSpeaker = document.createElement('option');
-            let speaker = speakerEntry.value;
-            newSpeaker.text = speaker;
-            newSpeaker.value = speaker;
-            entrySpeaker.appendChild(newSpeaker);
-        }
+        addValueToDropdown(entrySpeaker,speakerEntry);
+        let newOutput = document.createElement('div');
+        newOutput.textcontent = entrySpeaker.value;
+        let deleteButton = document.createElement('button');
+        deleteButton.text = '-'
+        newOutput.appendChild(deleteButton);
+        speakers.appendChild(newOutput);
+        deleteButton.addEventListener('click', function(e) {
+            newOutput.remove();
+        });
     });
+}
+
+function addValueToDropdown(dropdown,entry) {
+    if (dropdown.value.trim() === "") {
+        alert(dropdown.text + 'is not valid');
+    } else {
+        let newSpeaker = document.createElement('option');
+        let speaker = entry.value;
+        newSpeaker.text = speaker;
+        newSpeaker.value = speaker;
+        dropdown.appendChild(newSpeaker);
+    }
+    entry.value == '';
 }
