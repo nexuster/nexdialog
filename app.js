7ef9ce7e0a007fc026da6cad9f8d84bc6a1d3bb0
nexuster <coralpinklmao@outlook.com>
2025-03-24 09:13:43 -0400

    a

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ window.onload = function() {
     const entrySpeaker = document.querySelector('#entrySpeaker');
 
     submitButton.addEventListener('click', function(e) {
-        if (trim(entrySpeaker.textcontent) != "") {
+        //if (trim(entrySpeaker.textcontent) != "") {
             let newSpeaker = document.createElement('option');
             let speaker = speakerEntry.textcontent;
-            newSpeaker.textcontent = speaker;
+            newSpeaker.text = speaker;
             newSpeaker.value = speaker;
             entrySpeaker.add(speakerEntry);
-        } else {
-            alert(entrySpeaker.text + 'is not a valid speaker') //i HATE this
-        }
+        // } else {
+        //     alert(entrySpeaker.text + 'is not a valid speaker') //i HATE this
+        // }
     });
 }
